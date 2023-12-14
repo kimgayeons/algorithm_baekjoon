28e9269c29b8967886227df37007886fb8d3fdd4
kimgayeons <4402aaa@gmail.com>
2023-12-14 15:38:12 +0900

    2562번(최대값)

---
+++ src/main/java/org/example/step03/No2562.java
@@ -0,0 +1,25 @@
+package org.example.step03;
+
+import java.util.Scanner;
+
+public class No2562 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int arr[] = new int[9];
+        for (int i = 0; i < 9; i++) {
+            arr[i] += sc.nextInt();
+        }
+        sc.close();
+
+        int maxIndex = 0;
+        int max = arr[0];
+        for (int i = 0; i < 9; i++) {
+            if(max < arr[i]){
+                max = arr[i];
+                maxIndex = i;
+            }
+        }
+        System.out.println(max);
+        System.out.println(maxIndex+1);
+    }
+}
