1d8ccde91f202460fa9bbd7e22030989d24e066d
kimgayeons <4402aaa@gmail.com>
2023-12-27 14:13:09 +0900

    3743(단어 길이 대기)

---
+++ src/main/java/org/example/step05/No2743.java
@@ -0,0 +1,11 @@
+package org.example.step05;
+
+import java.util.Scanner;
+
+public class No2743 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String x = sc.nextLine();
+        System.out.println(x.length());
+    }
+}
